6d14b0d8d333a202af399d1e027776120ed16cca
Timu <55018078+hatimfilali@users.noreply.github.com>
2023-09-26 08:28:45 +0000

    finished

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,14 +6,14 @@ int main (int arc, char **arv)
     int		i;
 	int		j;
 
-	if (argc < 2)
+	if (arc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	j = -1;
-	while (argc > 1 && argv && argv[++j])
+	while (arc > 1 && arv && arv[++j])
 	{
 		i = -1;
-		while (j > 0 && argv[j][++i])
-			std::cout << (char)std::toupper(argv[j][i]);
+		while (j > 0 && arv[j][++i])
+			std::cout << (char)std::toupper(arv[j][i]);
 	}
 	std::cout << std::endl;
 	return (0);
